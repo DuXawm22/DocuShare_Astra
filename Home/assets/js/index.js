8323f267a8196f86826e29df0c704c78acd88d20
DuXawm22 <prawiravincent98@gmail.com>
2019-03-12 10:00:26 +0700

    styling

--- Home/assets/js/index.js
+++ Home/assets/js/index.js
@@ -10,24 +10,12 @@ $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
     $('.dropdown-submenu .show').removeClass("show");
   });
 
-
   return false;
 });
-var myNav = document.getElementById('mynav');
-window.onscroll = function () { 
-    "use strict";
-    if (document.body.scrollTop >= 200 ) {
-        myNav.classList.add("top-bar-colored");
-        myNav.classList.remove("top-bar-transparent");
-    } 
-    else {
-        myNav.classList.add("top-bar-transparent");
-        myNav.classList.remove("top-bar-colored");
-    }
-};
-// $(function () {
-//   $(document).scroll(function () {
-// 	  var $nav = $(".navbar-dark.scrolled");
-// 	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-// 	});
-// });
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
